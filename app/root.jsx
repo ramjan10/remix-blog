effadff39cd140106fb5cc4bc745278977473729
ramjan10 <ramjan@efoli.com>
2023-11-29 18:27:00 +0600

    remix app using javascript template

--- app/root.jsx
+++ app/root.jsx
@@ -1,32 +1,48 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { Link, LiveReload, Outlet } from "@remix-run/react";
+import globalStylesUrl from "./styles/global.css";
 
-export const links = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+//export const links = () => [{ rel: "stylesheet", href: globalStylesUrl }];
 
 export default function App() {
   return (
+    <Document>
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Document>
+  );
+}
+
+function Document({ children, title }) {
+  return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
+        <title>{title ? title : "My remix blog"}</title>
+        <link rel="stylesheet" href={globalStylesUrl} />
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
+        {children}
+        {process.env.NODE_ENV === "development" ? <LiveReload /> : null}
       </body>
     </html>
+  );
+}
+
+function Layout({ children }) {
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="logo">
+          Remix
+        </Link>
+
+        <ul className="nav">
+          <li>
+            <Link to="/posts">Posts</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="container">{children}</div>
+    </>
   );
 }
